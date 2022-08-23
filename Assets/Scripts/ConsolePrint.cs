e5f2f015501f579cdf3c88e2c7c3254faab15e54
Peaknight <pk200182@gmail.com>
2022-08-23 20:45:07 +0800

    80% Second time

--- Assets/Scripts/ConsolePrint.cs
+++ Assets/Scripts/ConsolePrint.cs
@@ -4,25 +4,20 @@ using UnityEngine;
 
 public class ConsolePrint : MonoBehaviour
 {
-    public GameObject redObj;
-    [SerializeField] private GameObject blueObj;
 
     // Start is called before the first frame update
     void Start()
     {
-        redObj.name = "RedPrefab";
-        blueObj.name = "BluePrefab";
-    
         
     }
 
     // Update is called once per frame
     void Update()
     {
-       int i = 3;
+        int i = 3;
         i++;
-        Debug.Log(redObj.name +":"+ i);
-        Debug.Log(blueObj.name +":"+ i);
+        Debug.Log(gameObject.name +":"+ i);
+
  
 
     }
